3ae7a81bc90528a5610dd2b9b9f42fcd4be536fd
Amine <wissarmohamedamine@gmail.com>
2023-11-11 19:07:11 +0100

    the component of testemonials is ended

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,82 @@
 import React from "react";
+import IMG1 from "../../assets/Portfolio1.png";
+import IMG2 from "../../assets/Portfolio2.png";
+import IMG3 from "../../assets/Portfolio3.png";
+import IMG4 from "../../assets/Portfolio4.jpg";
+import IMG5 from "../../assets/Portfolio5.png";
+import IMG6 from "../../assets/Portfolio6.jpg";
 import "./portfolio.css";
 const Portfolio = () => {
-  return <section id="portfolio">Portfolio</section>;
+  const data = [
+    {
+      id: 1,
+      image: IMG1,
+      title: "bimo",
+      github: "https://github.com",
+      demo: "https://dribbble.com/Alien_pixels",
+    },
+    {
+      id: 2,
+      image: IMG2,
+      title: "bimo",
+      github: "https://github.com",
+      demo: "https://dribbble.com/Alien_pixels",
+    },
+    {
+      id: 3,
+      image: IMG3,
+      title: "bimo",
+      github: "https://github.com",
+      demo: "https://dribbble.com/Alien_pixels",
+    },
+    {
+      id: 4,
+      image: IMG4,
+      title: "bimo",
+      github: "https://github.com",
+      demo: "https://dribbble.com/Alien_pixels",
+    },
+    {
+      id: 5,
+      image: IMG5,
+      title: "bimo",
+      github: "https://github.com",
+      demo: "https://dribbble.com/Alien_pixels",
+    },
+    {
+      id: 6,
+      image: IMG6,
+      title: "bimo",
+      github: "https://github.com",
+      demo: "https://dribbble.com/Alien_pixels",
+    },
+  ];
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className="btn" target="_blank">
+                  Github
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,71 @@
 import React from "react";
+import AVTR1 from "../../assets/avatar1.png";
+import AVTR2 from "../../assets/avatar2.png";
+import AVTR3 from "../../assets/avatar3.png";
+import AVTR4 from "../../assets/avatar4.png";
 import "./testimonials.css";
+// import Swiper core and required modules
+import { Pagination } from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
 
 const Testimonials = () => {
-  return <section id="testimonials">Testimonials</section>;
+  const data = [
+    {
+      avatar: AVTR1,
+      name: "Lara Smith",
+      review:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, nisi?",
+    },
+    {
+      avatar: AVTR2,
+      name: "Tom Thum",
+      review:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, nisi?",
+    },
+    {
+      avatar: AVTR3,
+      name: "Mario Olipo",
+      review:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, nisi?",
+    },
+    {
+      avatar: AVTR4,
+      name: "Justin Fridg",
+      review:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, nisi?",
+    },
+  ];
+  return (
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+      <Swiper
+        className="testimonial__container"
+        // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar} />
+              </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
 };
 
 export default Testimonials;
